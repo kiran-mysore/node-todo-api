9308b51255e82b024f093c3671394c7886bf29cc
kiran-mysore <kiran.mysore.rao@gmail.com>
2017-03-20 12:24:54 +0100

    Fixed the issues in the mongoose.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -6,7 +6,7 @@ let dbURI = {
     localhost:'mongodb://localhost:27017/TodoApp',
     mlab:'mongodb://todouser:todouser123@ds135680.mlab.com:35680/tododb'
 }
-mongoose.connect(dbUri.localhost || dbURI.mlab)
+mongoose.connect(dbURI.localhost || dbURI.mlab)
 
 module.exports= {
     mongoose
